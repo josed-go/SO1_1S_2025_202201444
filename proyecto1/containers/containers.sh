4bb8cf2391607759c90892ef267309624ccaad8b
josed-go <jdavidgongorao@gmail.com>
2025-03-03 22:27:49 -0600

    Se creo la carpeta para el proyecto 1 y se creo el script para crear contenedores

---
+++ proyecto1/containers/containers.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Función para generar un nombre aleatorio para los contenedores
+generate_name() {
+    echo "container_$(date +%s)_$RANDOM"
+}
+
+# Lista de imágenes a utilizar
+IMAGES=(
+    "containerstack/alpine-stress:latest"
+)
+
+# Cantidad de contenedores a generar
+NUM_CONTAINERS=10
+
+for ((i=0; i<$NUM_CONTAINERS; i++)); do
+    # Seleccionar un tipo de contenedor aleatorio
+    TYPE=$((RANDOM % 4))
+    
+    # Generar un nombre único
+    CONTAINER_NAME=$(generate_name)
+
+    case $TYPE in
+        0)
+            echo "Creando contenedor de RAM (moderado): $CONTAINER_NAME"
+            docker run -d --name "$CONTAINER_NAME" --memory="128m" "${IMAGES[0]}" stress --vm 1 --vm-bytes 64M --timeout 60s
+            ;;
+        1)
+            echo "Creando contenedor de CPU (moderado): $CONTAINER_NAME"
+            docker run -d --name "$CONTAINER_NAME" --cpus="0.5" "${IMAGES[0]}" stress --cpu 1 --timeout 60s
+            ;;
+        2)
+            echo "Creando contenedor de I/O (moderado): $CONTAINER_NAME"
+            docker run -d --name "$CONTAINER_NAME" "${IMAGES[0]}" stress --io 2 --timeout 60s
+            ;;
+        3)
+            echo "Creando contenedor de Disco (moderado): $CONTAINER_NAME"
+            docker run -d --name "$CONTAINER_NAME" "${IMAGES[0]}" stress --hdd 1 --hdd-bytes 64M --timeout 60s
+            ;;
+    esac
+done
